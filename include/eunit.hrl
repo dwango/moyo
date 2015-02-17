c78be47d0e0220d5d9fbcbde8e4b4139f839770e
Takeru Ohta <takeru_ohta@dwango.co.jp>
2015-02-17 17:54:36 +0900

    Add eunit helper macros

---
+++ include/eunit.hrl
@@ -0,0 +1,145 @@
+%% @copyright 2013-2015 DWANGO Co., Ltd. All Rights Reserved.
+%%
+%% @doc eunit用の追加マクロ
+%%
+%% このファイルをincludeすれば`eunit/include/eunit.hrl'でdefineされているマクロと合わせて使用ができる.
+
+-ifndef(MOYO_EUNIT_HRL).
+-define(MOYO_EUNIT_HRL, true).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-ifdef(NOASSERT).
+-define(assertMatch2(Guard, Expr), ok).
+-else.
+-define(assertMatch2(Guard, Expr),
+        (fun() ->
+                 try
+                     Guard = Expr,
+                     ok
+                 catch
+                     error:{badmatch, __MOYO_EUNIT_V} ->
+                         erlang:error({assertMatch_failed,
+                                       [{module, ?MODULE},
+                                        {line, ?LINE},
+                                        {expression, (??Expr)},
+                                        {pattern, (??Guard)},
+                                        {value, (__MOYO_EUNIT_V)}
+                                       ]})
+                 end
+         end)()).
+-endif.
+-define(_assertMatch2(Guard, Expr), ?_test(?assertMatch2(Guard, Expr))).
+%% ?assertMatch(_, Value) は書くことができない. <br />
+%% assertマクロを書く際にこれがあると便利なので定義している <br />
+%% 基本的な動作はassertMatchと同じ.
+
+-ifdef(NOASSERT).
+-define(assignMatch(Guard, Expr), Guard = Expr).
+-else.
+-define(assignMatch(Guard, Expr),
+        begin
+            Guard = (fun() ->
+                             __MOYO_EUNIT_TMP = Expr,
+                             ?assertMatch2(Guard, __MOYO_EUNIT_TMP),
+                             __MOYO_EUNIT_TMP
+                     end)()
+        end).
+-endif.
+-define(_assignMatch(Guard, Expr), ?_test(?assignMatch(Guard, Expr))).
+%% 以下の構文と同義のことが実現できる
+%% ```
+%% ?assertMatch({ok, _}, Ret),
+%% {ok, Pid} = Ret
+%% '''
+
+-ifdef(NOASSERT).
+-define(assertTerminated(Pid, Reason), ok).
+-else.
+-define(assertTerminated(Pid, Reason),
+        (fun() ->
+                 receive
+                     {'EXIT', Pid, __MOYO_EUNIT_RetReason} ->
+                         ?assertMatch2(Reason, __MOYO_EUNIT_RetReason)
+                 after 50 ->
+                         ?assert(timeout)
+                 end
+         end)()).
+-endif.
+-define(_assertTerminated(Pid, Reason), ?_test(?assertTerminated(Pid, Reason))).
+%% プロセスから'EXIT'が一定時間以内に投げられることを確認する.
+%%
+%% ```
+%% _   = process_flag(trap_exit, true),
+%% Pid = spawn_link(fun hoge/0),
+%% ?assertTerminated(Pid, _)
+%% '''
+
+-ifdef(NOASSERT).
+-define(assertDown(Ref, Reason), ok).
+-else.
+-define(assertDown(Ref, Reason),
+        (fun() ->
+                 receive
+                     {'DOWN', Ref, process, _, __MOYO_EUNIT_RetReason} ->
+                         ?assertMatch2(Reason, __MOYO_EUNIT_RetReason)
+                 after 50 ->
+                         ?assert(timeout)
+                 end
+         end)()).
+-endif.
+-define(_assertDown(Ref, Reason), ?_test(?assertDown(Ref, Reason))).
+%% 'DOWN'が一定時間以内に投げられることを確認する.
+%%
+%% ```
+%% {_, Ref} = spawn_monitor(fun hoge/0),
+%% ?assertDown(Ref, _)
+%% '''
+
+-ifdef(NOASSERT).
+-define(ensureExited(Pid, Reason), ok).
+-else.
+-define(ensureExited(Pid, Reason),
+        (fun() ->
+                 __MOYO_EUNIT_Old = process_flag(trap_exit, true),
+                 __MOYO_EUNIT_Ref = monitor(process, Pid),
+                 exit(Pid, Reason),
+                 ?assertDown(__MOYO_EUNIT_Ref, _),
+                 process_flag(trap_exit, __MOYO_EUNIT_Old)
+         end)()).
+-endif.
+-define(_ensureExited(Pid, Reason), ?_test(?ensureExited(Pid, Reason))).
+%% プロセスが生きていたら`Reason'という理由で終了させる.(`exit/2'を発行する)
+%% 一定時間以内に終了できなかった場合はtest failureになる.
+%%
+%% ```
+%% Pid = spwan(fun hoge/0),
+%% ?ensureExited(Pid)
+%% '''
+
+-define(ensureExited(Pid), ?ensureExited(Pid, kill)).
+-define(_ensureExited(Pid), ?_test(?ensureExited(Pid))).
+%% @equiv ?ensureExited(Pid, kill)
+
+-ifdef(NOASSERT).
+-define(assertLinked(Pid1, Pid2), ok).
+-else.
+-define(assertLinked(Pid1, Pid2),
+        (fun () ->
+                 {_, __Links} = erlang:process_info(Pid2, links),
+                 ?assert(lists:member(Pid1, __Links))
+         end)()).
+-endif.
+-define(_assertLinked(Pid1, Pid2), ?_test(?assertLinked(Pid1, Pid2))).
+%% 二つのプロセス(`Pid1'と`Pid2')の間にリンクが貼られているかを確認する.
+%%
+%% ```
+%% Pid = spawn_link(fun hoge/0),
+%% ?assertLinked(self(), Pid).
+%% '''
+
+-define(assertLinked(Pid), ?assertLinked(self(), Pid)).
+-define(_assertLinked(Pid), ?_test(?assertLinked(Pid))).
+%% @equiv ?assertLinked(self(), Pid)
+
+-endif. % ifndef MOYO_EUNIT_HRL
---
+++ test/moyo_eunit_tests.erl
@@ -0,0 +1,138 @@
+%% coding: latin-1
+%% @copyright 2013-2015 DWANGO Co., Ltd. All Rights Reserved.
+-module(moyo_eunit_tests).
+
+-include("eunit.hrl").
+%% 従来であれば
+%% -include_lib("moyo/include/eunit.hrl").
+
+moyo_eunit_test_() ->
+    {foreach,
+     fun()    -> process_flag(trap_exit, false) end,
+     fun(Old) -> process_flag(trap_exit, Old) end,
+     [
+      {"assertMatch2が正しく動作する",
+       fun() ->
+               ?assertMatch2(_, hoge),
+               ?assertMatch2(hoge, hoge),
+               ?assertError({assertMatch_failed, _}, ?assertMatch2([], lists:seq(1,3)))
+       end},
+      {"assertTerminatedが正常に実行できる",
+       fun() ->
+               _ = process_flag(trap_exit, true),
+               Pid = spawn_link(fun() -> exit(abort) end),
+               ?assertTerminated(Pid, abort)
+       end},
+      {"assertTerminatedが2回使用できる",
+       fun() ->
+               _ = process_flag(trap_exit, true),
+
+               Pid = spawn_link(fun() -> exit(abort) end),
+               ?assertTerminated(Pid, abort),
+
+               Pid2 = spawn_link(fun() -> exit(abort) end),
+               ?assertTerminated(Pid2, abort)
+       end},
+      {"assertDownが正常に実行できる",
+       fun() ->
+               {Pid, Ref} = spawn_monitor(fun() -> exit(abort) end),
+               ?assertDown(Ref, abort),
+               ?assertNot(is_process_alive(Pid))
+       end},
+      {"assertDownが2回使用できる",
+       fun() ->
+               {Pid, Ref} = spawn_monitor(fun() -> exit(abort) end),
+               ?assertDown(Ref, abort),
+               ?assertNot(is_process_alive(Pid)),
+
+               {Pid2, Ref2} = spawn_monitor(fun() -> exit(abort) end),
+               ?assertDown(Ref2, abort),
+               ?assertNot(is_process_alive(Pid2))
+       end},
+      {"ensureExitedが正常に実行できる",
+       fun() ->
+               Pid = spawn(fun() -> timer:sleep(infinity) end),
+               ?ensureExited(Pid),
+               ?assertNot(is_process_alive(Pid))
+       end},
+      {"プロセスの終了理由が指定できる",
+       fun() ->
+               Pid = spawn(fun() -> timer:sleep(infinity) end),
+               Monitor = monitor(process, Pid),
+               ?ensureExited(Pid, hoge),
+               receive
+                   {'DOWN', Monitor, _, _, Reason} -> ?assertEqual(hoge, Reason)
+               after 0 -> ?assert(timeout)
+               end
+       end},
+      {"ensureExitedはlinkしていても親プロセスが落ちない",
+       fun() ->
+               Pid = spawn_link(fun() -> timer:sleep(infinity) end),
+               ?ensureExited(Pid),
+               ?assertNot(is_process_alive(Pid))
+       end},
+      {"ensureExitedが2回使用できる",
+       fun() ->
+               Pid  = spawn(fun() -> timer:sleep(infinity) end),
+               ?ensureExited(Pid),
+               ?assertNot(is_process_alive(Pid)),
+
+               Pid2 = spawn(fun() -> timer:sleep(infinity) end),
+               ?ensureExited(Pid2),
+               ?assertNot(is_process_alive(Pid2))
+       end},
+      {"assignMatchが正常に利用できる",
+       fun() ->
+               L = lists:seq(1, 5),
+               ?assignMatch([H | _], L),
+               ?assertEqual(H, 1),
+
+               L2 = lists:seq(1, 0),
+               ?assertError({assertMatch_failed, _}, ?assignMatch([H2 | _], L2))
+       end},
+      {"assignMatchが部分が単なる代入でなくても利用できる",
+       fun() ->
+               [1 | T] = lists:seq(1,5),
+               ?assignMatch([1 | T], lists:seq(1,5)),
+               ?assignMatch([1 | T2], lists:seq(1,5)),
+               ?assertEqual(T, T2)
+       end},
+      {"assignMatchの引数に関数を入れた時、2回実行されない",
+       fun() ->
+               Fun = fun() -> self() ! ok end,
+               ?assignMatch(ok, Fun()),
+
+               ReceiveCounter = fun Counter(Count) -> receive ok -> Counter(Count + 1) after 0 -> Count end end,
+               ?assertEqual(1, ReceiveCounter(0))
+       end},
+      {"assignMatchで変数が既に束縛されているときも利用できる",
+       fun() ->
+               H = 1,
+               L = lists:seq(1, 5),
+               ?assignMatch([H | _], L)
+       end},
+      {"自プロセスと指定のプロセスの間にリンクが貼られているかどうかを確認できる",
+       fun () ->
+               Pid = spawn(timer, sleep, [infinity]),
+               ?assertError(_, ?assertLinked(Pid)), % not-linked,
+
+               true = link(Pid),
+               ?assertLinked(Pid) % linked
+       end},
+      {"指定のプロセス間にリンクが貼られているかどうかを確認できる",
+       fun () ->
+               Parent = self(),
+
+               Pid1 = spawn(timer, sleep, [infinity]),
+               Pid2 = spawn(timer, sleep, [infinity]),
+               ?assertError(_, ?assertLinked(Pid1, Pid2)), % not-linked,
+
+               Pid3 = spawn(fun () ->
+                                    true = link(Pid1),
+                                    Parent ! linked,
+                                    timer:sleep(infinity)
+                            end),
+               receive linked -> ok end,
+               ?assertLinked(Pid1, Pid3) % linked
+       end}
+     ]}.
